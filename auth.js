02e483cead9679c72c400a5998eab24f66d1cf8c
Omotayo Kolawole <omotayokolawole68@gmail.com>
2026-03-07 22:22:28 +0100

    Fix database path in auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
-const db = require('../db');
+const db = require('./db');
 const { issueToken, verifyJWT } = require('../middleware/auth');
 
 const router = express.Router();

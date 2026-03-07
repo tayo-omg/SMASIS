3b9de69a956f7183cb95413d5f9b603481c50228
Omotayo Kolawole <omotayokolawole68@gmail.com>
2026-03-07 23:45:18 +0100

    Update import path for auth middleware

--- reports.js
+++ reports.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const db = require('./db');
-const { verifyJWT, requireRole } = require('../middleware/auth');
+const { verifyJWT, requireRole } = require('./middleware/auth');
 
 const router = express.Router();
 

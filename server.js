56b40b8bdc646e0eab7ee25f1cc50cf60fa41cf7
Omotayo Kolawole <omotayokolawole68@gmail.com>
2026-03-15 15:21:44 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // MIDDLEWARE
 app.use(cors({
-  origin: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+  origin: process.env.CLIENT_ORIGIN || '*',
   credentials: true,
 }));
 app.use(express.json({ limit: '10mb' }));
